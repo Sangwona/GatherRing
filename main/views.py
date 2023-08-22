6589bb44cf57065a45a778b3f28a3d50e4338ed1
carolina <carolina.rojas1295@gmail.com>
2023-08-21 18:16:06 -0700

    [feat] Add navbar

--- main/views.py
+++ main/views.py
@@ -4,10 +4,4 @@ from django.urls import reverse
 # Create your views here.
 
 def index(request):
-    # Authenticated users view the main page
-    if request.user.is_authenticated:
-        return render(request, "main/layout.html")
-
-    # Everyone else is prompted to sign in
-    else:
-        return HttpResponseRedirect(reverse("login"))
+    return render(request, "main/index.html")
