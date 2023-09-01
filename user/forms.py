71613d0a74ee975daa3b5fe148be32075575b96d
carolina <carolina.rojas1295@gmail.com>
2023-08-31 22:13:59 -0700

    [style] add bootstrap for user forms

--- user/forms.py
+++ user/forms.py
@@ -3,22 +3,36 @@ from django import forms
 from .models import User
 
 class RegisterForm(UserCreationForm):
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Confirm Password'}))
+
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'type':'text', 'class':'form-control', 'placeholder':'Username'})
+        }
 
 class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
     class Meta:
         model = User
         fields = ['username', 'password']
 
 class EditUserForm(forms.ModelForm):
-    new_password1 = forms.CharField(label="New Password", widget=forms.PasswordInput, required=False)
-    new_password2 = forms.CharField(label="Confirm New Password", widget=forms.PasswordInput, required=False)
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}), required=False)
+    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}), required=False)
 
     class Meta:
         model = User
         fields = ['photo', 'bio', 'interests', 'location']
+        widgets = {
+            'photo': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+            'bio': forms.Textarea(attrs={'rows':'4', 'class':'form-control', 'placeholder':'A few words about you ...'}),
+            'interests': forms.SelectMultiple(attrs={'class':'form-control'}),
+            'location': forms.TextInput(attrs={'id': 'location-name', 'type':'text', 'class':'form-control', 'placeholder':'Location'}),
+        }
         
     def clean(self):
         cleaned_data = super().clean()
