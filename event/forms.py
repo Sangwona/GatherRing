aaf30bd7c9c61ac7c4c143f5f0dfd0fc946d68ec
carolina <carolina.rojas1295@gmail.com>
2023-09-01 20:11:55 -0700

    [style] add bootstrap for event forms

--- event/forms.py
+++ event/forms.py
@@ -2,47 +2,46 @@ from django import forms
 from .models import Event, GroupEvent
 
 class CreateEventForm(forms.ModelForm):            
-    start_time = forms.DateTimeField(
-        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-        input_formats=['%Y-%m-%dT%H:%M']
-    )
-    end_time = forms.DateTimeField(
-        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-        input_formats=['%Y-%m-%dT%H:%M']
-    )
     class Meta:
         model = Event
         exclude = ['cover_photo', 'status', 'created_at', 'creator', 'hosts', 'attendees', 'photos']
         widgets = {
-            'location': forms.TextInput(attrs={'id': 'api-location', 'type':'text', 'class':'form-control', 'placeholder':'Location'}),
-            'location_lat' : forms.HiddenInput(attrs={'id': 'location-lat'}),
-            'location_lng' : forms.HiddenInput(attrs={'id': 'location-lng'}),
-        }
-
-class CreateGroupEventForm(CreateEventForm):            
-    class Meta:
-        model = GroupEvent
-        exclude = ['cover_photo', 'status', 'location_lat', 'location_lng', 'created_at', 'creator', 'attendees', 'photos', 'group', 'hosts']
-
-class EditEventForm(forms.ModelForm):            
-    class Meta:
-        model = Event
-        exclude = ['created_at', 'creator', 'attendees', 'photos']
-        widgets = {
-            'name': forms.TextInput(attrs={'type':'text', 'class':'form-control', 'placeholder':'Title'}),
+            'name': forms.TextInput(attrs={'type':'text', 'class':'form-control', 'placeholder':'Name'}),
             'description': forms.Textarea(attrs={'rows':'5', 'class':'form-control', 'placeholder':'Describe the event ...'}),
+            'visibility': forms.RadioSelect(),
+            'join_mode': forms.RadioSelect(),
+            'capacity': forms.NumberInput(attrs={'class': 'form-control', 'value': 50}),
             'location': forms.TextInput(attrs={'id': 'api-location', 'type':'text', 'class':'form-control', 'placeholder':'Location'}),
             'location_lat' : forms.HiddenInput(attrs={'id': 'location-lat'}),
             'location_lng' : forms.HiddenInput(attrs={'id': 'location-lng'}),
             'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class':'form-control'}),
             'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class':'form-control'}),
+            'interests': forms.CheckboxSelectMultiple() 
+        }
+
+class CreateGroupEventForm(CreateEventForm):            
+    class Meta:
+        model = GroupEvent
+        exclude = ['cover_photo', 'status', 'created_at', 'creator', 'hosts', 'attendees', 'photos', 'group']
+
+class EditEventForm(forms.ModelForm):            
+    class Meta:
+        model = Event
+        exclude = ['status', 'created_at', 'creator', 'attendees', 'photos']
+        widgets = {
             'cover_photo': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
-            'max_members': forms.NumberInput(attrs={'class': 'form-control'}),
-            'visibility': forms.Select(attrs={'class':'form-control'}),
-            'join_mode': forms.Select(attrs={'class':'form-control'}),
-            'status': forms.Select(attrs={'class':'form-control'}),
-            'hosts': forms.SelectMultiple(attrs={'class':'form-control'}),
-            'categories': forms.SelectMultiple(attrs={'class':'form-control'}),
+            'name': forms.TextInput(attrs={'type':'text', 'class':'form-control', 'placeholder':'Name'}),
+            'description': forms.Textarea(attrs={'rows':'5', 'class':'form-control', 'placeholder':'Describe the event ...'}),
+            'visibility': forms.RadioSelect(),
+            'join_mode': forms.RadioSelect(),
+            'capacity': forms.NumberInput(attrs={'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'id': 'api-location', 'type':'text', 'class':'form-control', 'placeholder':'Location'}),
+            'location_lat' : forms.HiddenInput(attrs={'id': 'location-lat'}),
+            'location_lng' : forms.HiddenInput(attrs={'id': 'location-lng'}),
+            'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class':'form-control'}),
+            'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class':'form-control'}),
+            'hosts': forms.CheckboxSelectMultiple(),
+            'interests': forms.CheckboxSelectMultiple()
         }
 
 class EditGroupEventForm(EditEventForm):            
