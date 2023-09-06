30edb89bbaeab88005da395b31e664670672d6ec
Sangwon <aswton@gmail.com>
2023-09-06 12:09:38 -0700

    [fix] update bio migration

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     photo =models.ImageField(upload_to="user_profile_photos/", blank=True)
-    bio = models.TextField(max_length=500, blank=True)
+    bio = models.TextField(max_length=1000, blank=True)
     interests = models.ManyToManyField('main.Interest', blank=True)
     location = models.CharField(max_length=256, blank=True)
     location_lat = models.FloatField(null=True, blank=True)
