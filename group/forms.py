e3d2142c5449c0372de3de02af34b1d617ef1bc8
carolina <carolina.rojas1295@gmail.com>
2023-09-07 21:48:10 -0700

    [style] Create Group and Edit Group Forms

--- group/forms.py
+++ group/forms.py
@@ -1,41 +1,60 @@
 from django import forms
 from .models import Group
-from main.models import Interest
 
 class LocationForm1(forms.ModelForm):
-    location = forms.CharField(
-        widget=forms.TextInput(attrs={'autofocus': True, 'id': 'api-location'})
-    )
-    location_lat = forms.FloatField(required=False, widget=forms.HiddenInput(attrs={'id': 'location-lat'}))
-    location_lng = forms.FloatField(required=False, widget=forms.HiddenInput(attrs={'id': 'location-lng'}))
     class Meta:
         model = Group
         fields = ["location", "location_lat", "location_lng"]
+        widgets = {
+            'location': forms.TextInput(attrs={'id': 'api-location', 'type':'text', 'class':'form-control', 'placeholder':'Where is your group based?'}),
+            'location_lat' : forms.HiddenInput(attrs={'id': 'location-lat'}),
+            'location_lng' : forms.HiddenInput(attrs={'id': 'location-lng'})
+        }
 
 class InterestsForm2(forms.ModelForm):
     class Meta:
         model = Group
         fields = ["interests"]
+        labels  = {
+        'interests':'Choose some topics related to your group', 
+        }
+        widgets = {
+            'interests': forms.CheckboxSelectMultiple() 
+        }
 
 class NameForm3(forms.ModelForm):
     class Meta:
         model = Group
         fields = ["name"]
+        widgets = {
+            'name': forms.TextInput(attrs={'type':'text', 'class':'form-control', 'placeholder':'Give your group a name'})
+        }
 
 class DescriptionForm4(forms.ModelForm):
     class Meta:
         model = Group
         fields = ["description"]
+        widgets = {
+            'description': forms.Textarea(attrs={'rows':'5', 'class':'form-control', 'placeholder':'Briefly describe your group ...'})
+        }
 
 class EditGroupForm(forms.ModelForm):
-    location = forms.CharField(
-        widget=forms.TextInput(attrs={'autofocus': True, 'id': 'api-location'})
-    )
-    location_lat = forms.FloatField(required=False, widget=forms.HiddenInput(attrs={'id': 'location-lat'}))
-    location_lng = forms.FloatField(required=False, widget=forms.HiddenInput(attrs={'id': 'location-lng'}))
     class Meta:
         model = Group
         exclude = ["creator", "photos", "members", "created_at"]
+        widgets = {
+            'cover_photo': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+            'name': forms.TextInput(attrs={'type':'text', 'class':'form-control', 'placeholder':'Name'}),
+            'description': forms.Textarea(attrs={'rows':'5', 'class':'form-control', 'placeholder':'Describe your group ...'}),
+            'visibility': forms.RadioSelect(),
+            'join_mode': forms.RadioSelect(),
+            'capacity': forms.NumberInput(attrs={'class': 'form-control', 'value': 50}),
+            'location': forms.TextInput(attrs={'id': 'api-location', 'type':'text', 'class':'form-control', 'placeholder':'Location'}),
+            'location_lat' : forms.HiddenInput(attrs={'id': 'location-lat'}),
+            'location_lng' : forms.HiddenInput(attrs={'id': 'location-lng'}),
+            'interests': forms.CheckboxSelectMultiple(),
+            'admins': forms.CheckboxSelectMultiple(),
+        }
     
     def __init__(self, *args, **kwargs):
         group = kwargs.get('instance', None)
